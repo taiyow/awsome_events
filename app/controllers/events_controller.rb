e93ee7d69792ea84f0b199bdfb560c5b021e4ede
Taiyo Watanabe <taiyo.w@gmail.com>
2016-02-14 22:56:25 +0900

    add event edit page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -19,6 +19,19 @@ class EventsController < ApplicationController
     @event = Event.find(params[:id])
   end
 
+  def edit
+    @event = current_user.created_events.find(params[:id])
+  end
+
+  def update
+    @event = current_user.created_events.find(params[:id])
+    if @event.update(event_params)
+      redirect_to @event, notice: '変更しました'
+    else
+      render :edit
+    end
+  end
+
   private
 
   def event_params
