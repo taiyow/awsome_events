c91c0a5003c2f67488c722b41a43093d3fe8d128
Taiyo Watanabe <taiyo.w@gmail.com>
2016-02-15 22:01:27 +0900

    change migration file to add non-null restriction and index

--- db/migrate/20160215125738_create_tickets.rb
+++ db/migrate/20160215125738_create_tickets.rb
@@ -1,11 +1,16 @@
 class CreateTickets < ActiveRecord::Migration
   def change
     create_table :tickets do |t|
-      t.references :user, index: true, foreign_key: true
-      t.references :event, index: true, foreign_key: true
+      t.references :user
+      t.references :event, null: false
       t.string :comment
 
       t.timestamps null: false
     end
+
+    add_index :tickets, [ :user_id, :event_id ], unique: true
+    add_index :tickets, [ :event_id, :user_id ], unique: true
+
   end
+
 end
