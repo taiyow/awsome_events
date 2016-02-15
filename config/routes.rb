3593e24df14b2f02809b4e3d020c6ce6cc92d33f
Taiyo Watanabe <taiyo.w@gmail.com>
2016-02-15 22:35:36 +0900

    add link to create event ticket, add routing

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   get '/auth/:provider/callback' => 'sessions#create'
   get '/logout' => 'sessions#destroy', as: :logout
 
+  resources :events do
+    resources :tickets
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
