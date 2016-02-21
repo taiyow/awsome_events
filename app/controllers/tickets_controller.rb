64f8cfb39f60a945fd22542fb21041d71299822b
Taiyo Watanabe <taiyo.w@gmail.com>
2016-02-21 21:55:34 +0900

    add "JOIN" feature

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,2 +1,27 @@
+# -*- coding: utf-8 -*-
 class TicketsController < ApplicationController
+  before_action :authenticate
+
+  def new
+    raise ActionController::RoutingError, 'ログイン状態で TicketsController#new にアクセス'
+  end
+
+  def create
+    ticket = current_user.tickets.build do |t|
+      t.event_id = params[:event_id]
+      t.comment = params[:ticket][:comment]
+    end
+    begin
+      if ticket.save
+        flash[:notice] = 'このイベントに参加表明しました'
+        head 201
+#      else
+#        binding.pry
+#        render json: { messages: ticket.errors.full_messages }, status: 422
+      end
+    rescue => e
+      # render json: { messages: ticket.errors.full_messages }, status: 422
+      render json: { messages: [e.message] }, status: 422
+    end
+  end
 end
